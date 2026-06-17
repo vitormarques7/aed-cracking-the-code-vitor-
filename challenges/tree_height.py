28813232a3bac4bfc725ebcb04deb8c8f837a377
Vitor <joaovitormarquesbr@gmail.com>
2026-06-17 11:29:24 -0300

    Resolve exercício 2: calcula altura da árvore

--- challenges/tree_height.py
+++ challenges/tree_height.py
@@ -2,4 +2,10 @@ from data_structures.node import Node
 
 
 def tree_height(root: Node | None) -> int:
-    raise NotImplementedError
+    if root is None:
+        return -1
+
+    left_height = tree_height(root.get_left_child())
+    right_height = tree_height(root.get_right_child())
+
+    return 1 + max(left_height, right_height)
